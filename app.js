2d8d17211c20e343d5f294e3e4ea591a2d59b930
officialdevking <kingsleysunday998@gmail.com>
2021-09-27 10:00:56 -0700

    Done for Now

--- app.js
+++ app.js
@@ -12,26 +12,67 @@ cancel.addEventListener('click', ()=>{
     document.getElementById('small_nav').classList.remove('small_nav_active')
 })
 
-const shorten = (e) => {
-    e.preventDefault();
 
-    let link = document.getElementById('main_link').value
-    // let shorten = document.getElementById('shorten').value
-    console.log(link);
-        fetch("https://capsulink.p.rapidapi.com/capsulate", {
-        "method": "POST",
+
+const link = document.getElementById('main_link');
+const shorten = document.getElementById('shorten');
+
+shorten.addEventListener('click', ()=>{
+    let urlshorten = link.value;
+
+        fetch("https://url-shortener9.p.rapidapi.com/", {
+        "method": "GET",
         "headers": {
-            "content-type": "application/json",
-            "api-key": "<Some API key>",
-            "x-rapidapi-host": "capsulink.p.rapidapi.com",
+            "x-rapidapi-host": "url-shortener9.p.rapidapi.com",
             "x-rapidapi-key": "df588214aamshd01fad0b7fabe01p1eb880jsn93719d08f942"
-        },
-        "body": {
-            "url": "https://github.com/officialdevking"
         }
-    }).then(response => response.json())
-    .then(data => console.log(data));
-    
-}
+    })
+    .then(response => {
+        response.json();
+        console.log(response);  
+    })
+    .then(data => {
+        console.log(data);
+    })
+    .catch(err => {
+        console.error(err);
+    });
+})
 
-document.getElementById('shorten').addEventListener('click', shorten)
+
+
+
+
+
+// const shorten = (e) => {
+//     e.preventDefault();
+
+//     // let data = new FormData()
+//     // data.append('URL', 'https://cloudinary.com/console/c-661efecee36b67d1c22af4f127fc0e/media_library/folders/bdfa10a597891de6caf2759ef24475e17d')
+
+//     let link = document.getElementById('main_link').value
+//     // let shorten = document.getElementById('shorten').value
+//     console.log(link);
+//         fetch("https://url-shortener-service.p.rapidapi.com/shorten", {
+//         "method": "POST",
+//         "headers": {
+//             "content-type": "application/x-www-form-urlencoded",
+//             "x-rapidapi-host": "url-shortener-service.p.rapidapi.com",
+//             "x-rapidapi-key": "df588214aamshd01fad0b7fabe01p1eb880jsn93719d08f942"
+//         },
+//         "body": {
+//             "url": "https:link"
+//         }
+//     })
+//     .then(response => {
+//         response.json();
+//     })
+//     .then(data => {
+//         console.log(data)
+//     })
+//     .catch(err => {
+//         console.error(err);
+//     });
+// }
+
+// document.getElementById('shorten').addEventListener('click', shorten)
