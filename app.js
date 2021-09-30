55bb7e5ae6bc83e3e94678e0b31e32835a25defe
officialdevking <kingsleysunday998@gmail.com>
2021-09-30 11:40:09 -0700

    Done for Now

--- app.js
+++ app.js
@@ -16,63 +16,51 @@ cancel.addEventListener('click', ()=>{
 
 const link = document.getElementById('main_link');
 const shorten = document.getElementById('shorten');
+const output = document.getElementById('output');
+let shortUrl = document.querySelector('.short_url');
+let longUrl = document.querySelector('.long_url');
 
-shorten.addEventListener('click', ()=>{
-    let urlshorten = link.value;
+let copyText = null
 
-        fetch("https://url-shortener9.p.rapidapi.com/", {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-host": "url-shortener9.p.rapidapi.com",
-            "x-rapidapi-key": "df588214aamshd01fad0b7fabe01p1eb880jsn93719d08f942"
-        }
+
+shorten.addEventListener('click', () =>{
+    
+    const urlshorten = link.value;
+
+    fetch(`https://api.shrtco.de/v2/shorten?url=${urlshorten}`, {
+        method: "GET"
     })
-    .then(response => {
-        response.json();
-        console.log(response);  
-    })
+    .then(response => response.json())
     .then(data => {
-        console.log(data);
-    })
-    .catch(err => {
-        console.error(err);
+        copyText = data.result.full_short_link; 
+        shortUrl.innerHTML = copyText; 
+        longUrl.innerHTML = data.result.original_link; 
+        btnLoad.classList.remove('load')
+        btnShorten.classList.remove('load_1')
+        output.classList.add('output')        
+        output.classList.remove('output_1')        
     });
+
+
+    let btnLoad = document.querySelector('.buttonload')
+    let btnShorten = document.querySelector('.shorten')
+    // let output = document.querySelector('.output');
+
+    if (urlshorten) {
+        btnLoad.classList.add('load')
+    }else{
+        alert("You've not inputted any link to be shortened!!!")
+    }
+
+    if (urlshorten) {
+        btnShorten.classList.add('load_1')
+    }
+
 })
 
+const copy = document.getElementById('copy');
 
 
-
-
-
-// const shorten = (e) => {
-//     e.preventDefault();
-
-//     // let data = new FormData()
-//     // data.append('URL', 'https://cloudinary.com/console/c-661efecee36b67d1c22af4f127fc0e/media_library/folders/bdfa10a597891de6caf2759ef24475e17d')
-
-//     let link = document.getElementById('main_link').value
-//     // let shorten = document.getElementById('shorten').value
-//     console.log(link);
-//         fetch("https://url-shortener-service.p.rapidapi.com/shorten", {
-//         "method": "POST",
-//         "headers": {
-//             "content-type": "application/x-www-form-urlencoded",
-//             "x-rapidapi-host": "url-shortener-service.p.rapidapi.com",
-//             "x-rapidapi-key": "df588214aamshd01fad0b7fabe01p1eb880jsn93719d08f942"
-//         },
-//         "body": {
-//             "url": "https:link"
-//         }
-//     })
-//     .then(response => {
-//         response.json();
-//     })
-//     .then(data => {
-//         console.log(data)
-//     })
-//     .catch(err => {
-//         console.error(err);
-//     });
-// }
-
-// document.getElementById('shorten').addEventListener('click', shorten)
+copy.addEventListener('click', ()=>{
+    navigator.clipboard.writeText(copyText);
+})
